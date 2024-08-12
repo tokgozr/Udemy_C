71de05a7536c135ec5adab4578ab46b066912e94
Rümeysa Tokgöz <126255709+tokgozr@users.noreply.github.com>
2024-08-12 15:26:13 +0300

    Create 157.c

---
+++ 157.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+//ekrana kelebek şeklinde yıldız basmak
+void sekilciz(int kenar, char karakter)
+{
+    int i,j;
+    for(j=kenar;j>0;j--)
+    {
+        for(i=1;i<=kenar-j;i++)
+        {
+            printf(" ");
+        }
+        for(i=1;i<=2*j-1;i++)
+        {
+            printf("%c",karakter);
+        }
+
+        printf("\n");
+    }
+
+    for(j=1;j<=kenar;j++)
+    {
+        for(i=1;i<=kenar-j;i++)
+        {
+            printf(" ");
+        }
+        for(i=1;i<=2*j-1;i++)
+        {
+            printf("%c",karakter);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int kenar;
+    char karakter;
+    printf("Lutfen basilacak karakteri giriniz.\n");
+    scanf("%c",&karakter);
+    printf("Kenar sayisi = ");
+    scanf("%d",&kenar);
+    printf("\n\n\n");
+    sekilciz(kenar,karakter);
+   return 0;
+}
